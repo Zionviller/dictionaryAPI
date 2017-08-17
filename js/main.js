863504fc5b518294639826a69c0a076f7cbe6e02
Chris Butler <zionviller@gmail.com>
2017-08-16 17:43:43 -0700

    Basically done..

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("Yeep!");
+//console.log("Yeep!");
 
 var xhr = new XMLHttpRequest();
 
@@ -10,6 +10,11 @@ var data;
 var definitions;
 var resultsDIV = document.getElementById('results');
 
+if(localStorage.search)
+{
+  searchBox.value = localStorage.search;
+}
+
 xhr.onreadystatechange = function() {
   if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
     data = JSON.parse(xhr.responseText);
@@ -18,23 +23,46 @@ xhr.onreadystatechange = function() {
 };
 
 goButton.addEventListener('mouseup', doSearch, false);
+searchBox.addEventListener('keyup', handleEnterKey, false);
 
 function doSearch() {
   var query = searchBox.value.replace(/[^a-zA-Z]/g, '');
-  console.log("Search is: " + query);
-
+  //console.log("Search is: " + query);
+  localStorage.search = query;
   xhr.open('GET', 'https://api.datamuse.com/words?rel_syn=' + query, true);
   xhr.send(null);
 }
 
+function handleEnterKey(e)
+{
+  //console.log('key released');
+  if(e.keyCode == 13)
+  {
+    doSearch();
+  }
+}
+
 function displayResults() {
-  if(data.length >= 5) {
+  ///console.log(data);
+  resultsDIV.innerHTML = '';
+  var resultsText = '';
+  if(data.length == 0)
+  {
+    resultsText = "Cannot find synonyms.";
+  } else if(data.length >= 5)
+  {
     for(var i = 0; i < 5; i++) {
-      console.log(data[i]);
+      resultsText += "<p>";
+      resultsText += data[i].word;
+      resultsText += "</p>";
     }
   } else {
     for(var i = 0; i < data.length; i++){
-      console.log(data[i]);
+      resultsText += "<p>";
+      resultsText += data[i].word;
+      resultsText += "</p>";
     }
   }
+
+  resultsDIV.innerHTML = resultsText;
 }
